3719285217cf43e86e214f0c6e1d0421765c4ab3
HangwelaniN <hngwekhulu@gmail.com>
2024-01-22 15:03:43 +0200

    main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
-    <h1>hello world</h1>
-    </div>
+      <div className="apps">
+    <h1 className="d-flex justify-content-start fs-3 mx-2 p-3 fw-bolder">HangwelaniN</h1>
+     <div className='contain'>
+    <h2 className="head pb-3">Tell your story</h2>
+    <p>
+      Create a beautiful blog to share your passionate 
+      with the world. Start your free trial today.
+      No credit card required.
+      </p>
+      <br />
+      <Button variant="dark" className='p-3 m-3'>GET STARTED</Button>
+      <br />
+      <br />
+      <FontAwesomeIcon icon={faAngleDown} size="2xl" className='m-5' />
+      </div>
+      </div>
+      </div>
+     
   );
 }
 
